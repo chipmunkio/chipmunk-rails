58da8d1cb24a5fd3e68fabb44ef3fe8967c439b2
Spencer Schoeben <spencer@netspencer.com>
2013-02-23 13:34:01 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chipmunk::Application.config.secret_token = '644997842f24d02d289c0e6ac29c0c7523163f403300050bc2088a1c096b6cbf17853c5753fb7d92422483df1e100e0baf268555a9f2d4bac015b82ab2c92278'
