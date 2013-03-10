2b06f9d96275103b470e629e6dcbadcc60532c94
Amadou Crookes <ASCrookes@Gmail.com>
2013-03-10 18:35:50 -0400

    sql table join

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,36 +1,5 @@
 class Item < ActiveRecord::Base
-  attr_accessible :name, :minutes, :img_url, :image, :details_id, :details_type
-  
-  #has_attached_file :image, :styles => { :full => "310x148>" }, :default_url => "/images/:style/missing.png"
-  
-  belongs_to :details, :polymorphic => true  
-  
-  default_scope select("id, name, minutes, img_url, details_type")
-  default_scope order("minutes DESC, RANDOM()")
-  
-  scope :minutes
-  scope :location
-  scope :page
-  
-  def self.page(page_num = 1)
-    if page_num.nil?
-      page_num = 0
-    else
-      page_num = page_num.to_i
-      page_num = page_num - 1
-    end
-    offset_num = 10*page_num
-    limit(10).offset(offset_num)
-  end
-   
-  def self.minutes(minutes = nil)
-    unless minutes.nil?
-      where("minutes <= ?", minutes)
-    end
-  end
-  
-  def self.location(latitude, longitude)
-    
-  end
-  
+  attr_accessible :details_type, :img_url, :minutes, :name
+  has_one :link
+  has_one :venue
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,9 +1,4 @@
 class Venue < ActiveRecord::Base
-  attr_accessible :address, :latitude, :longitude, :item_attributes
-  
-  has_one :item, :as => :details
-  geocoded_by :address
-  after_validation :geocode
-  
-  accepts_nested_attributes_for :item
+  attr_accessible :address, :latitude, :longitude, :item_id
+  belongs_to :item, :foreign_key => "item_id"
 end
